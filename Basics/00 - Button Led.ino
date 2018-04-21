f1a26f26eaac236fe8aca4eb7c4831355fdab7f0
Ricardo Kim <hexadecimal.exe@gmail.com>
2018-04-21 18:53:49 -0300

    Create 00 - Button Led.ino

---
+++ Basics/00 - Button Led.ino
@@ -0,0 +1,19 @@
+#include<ESP8266WiFi.h>
+int Button = 0;
+int ButtonData;
+
+int Led = 2;
+
+void setup(){
+  //for debug  issues
+  Serial.begin(9600);
+  
+  //pin Setups
+  pinMode(Led,OUTPUT);
+  pinMode(Button,INPUT);
+}
+
+void loop(){
+  //make button Flash in esp8266 control the led in board ON/OFF
+  digitalWrite(Led,digitalRead(Button));
+}
